7873032d2fa1bf65801732c4b1d8b4e0970a4a92
ElectroniaDC <electronica.titan@gmail.com>
2025-09-20 16:06:32 -0300

    0.1

---
+++ ParOrdenado.java
@@ -0,0 +1,73 @@
+/**
+ *Tarea 10 del practico N1 (rama master)
+ */
+public class ParOrdenado {
+    
+    private int primerComponente;
+    private int segundoComponente;
+    
+    
+    /**
+     * Constructor for objects of class ParOrdenado
+     */
+    public ParOrdenado()
+    {
+        primerComponente = 0;
+        segundoComponente = 0;
+    
+    }
+    
+    public void cambiarPrimerComponete(int x){
+        primerComponente = x;
+    }
+    
+    public void cambiarSegundoComponente(int y){
+        segundoComponente = y;
+    }
+    
+        public int primerComponente(){
+        return primerComponente;
+    }
+    
+    public int getSegundoComponente(){
+        return segundoComponente;
+    }
+
+
+}/**
+ *Tarea 10 del practico N1 (rama master)
+  */
+public class ParOrdenado {
+        
+        private int primerComponente;
+            private int segundoComponente;
+                
+                
+                /**
+                 *      * Constructor for objects of class ParOrdenado
+                 *           */
+                public ParOrdenado()
+                        {
+                                    primerComponente = 0;
+                                            segundoComponente = 0;
+                                                
+                                                }
+                    
+                    public void cambiarPrimerComponete(int x){
+                                primerComponente = x;
+                                    }
+                        
+                        public void cambiarSegundoComponente(int y){
+                                    segundoComponente = y;
+                                        }
+                            
+                                public int primerComponente(){
+                                            return primerComponente;
+                                                }
+                                    
+                                    public int getSegundoComponente(){
+                                                return segundoComponente;
+                                                    }
+
+
+}
