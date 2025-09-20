90b6fc6d932e84b2be526621f2638eae0a0db361
ElectroniaDC <electronica.titan@gmail.com>
2025-09-20 16:13:11 -0300

    subiendo

--- ParOrdenado.java
+++ ParOrdenado.java
@@ -20,8 +20,7 @@ public class ParOrdenado {
     public void cambiarPrimerComponete(int x){
         primerComponente = x;
     }
-    
-    public void cambiarSegundoComponente(int y){
+        public void cambiarSegundoComponente(int y){
         segundoComponente = y;
     }
     
@@ -32,42 +31,6 @@ public class ParOrdenado {
     public int getSegundoComponente(){
         return segundoComponente;
     }
-
-
-}/**
- *Tarea 10 del practico N1 (rama master)
-  */
-public class ParOrdenado {
-        
-        private int primerComponente;
-            private int segundoComponente;
-                
-                
-                /**
-                 *      * Constructor for objects of class ParOrdenado
-                 *           */
-                public ParOrdenado()
-                        {
-                                    primerComponente = 0;
-                                            segundoComponente = 0;
-                                                
-                                                }
-                    
-                    public void cambiarPrimerComponete(int x){
-                                primerComponente = x;
-                                    }
-                        
-                        public void cambiarSegundoComponente(int y){
-                                    segundoComponente = y;
-                                        }
-                            
-                                public int primerComponente(){
-                                            return primerComponente;
-                                                }
-                                    
-                                    public int getSegundoComponente(){
-                                                return segundoComponente;
-                                                    }
 
 
 }
